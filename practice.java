e7ff73d983d733662ac982ffef1e53325de0df46
Pratik Bhure <pratikbhure653@gmail.com>
2023-08-04 14:58:21 +0530

    commited

---
+++ practice.java
@@ -0,0 +1,7 @@
+public class practice {
+    public static void main(String[] args) {
+        String str="abc";
+        String str2="abc";
+        System.out.println(str.compareTo(str2));
+    }
+}
